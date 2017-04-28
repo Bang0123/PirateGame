e2366a0a5a960aee4e20160130428523b9d3031f
Dan Bluhm Hansen <danx1314@edu.easj.dk>
2017-04-28 11:21:27 +0200

    ScoreController implemented, still needs to be enabled when the player dies.

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class ScoreController : MonoBehaviour
+    {
+		private Text _scoreText;
+
+		/// <summary>
+		/// Start is called on the frame when a script is enabled just before
+		/// any of the Update methods is called the first time.
+		/// </summary>
+		void Start()
+		{
+			_scoreText = gameObject.GetComponent<Text>();			
+		}
+
+		/// <summary>
+		/// This function is called when the object becomes enabled and active.
+		/// </summary>
+		public void OnEnable()
+		{
+			_scoreText.text = GameObject.FindWithTag("Player").GetComponent<PlayerController>().ScoreTime.ToString();
+		}
+    }
+}
