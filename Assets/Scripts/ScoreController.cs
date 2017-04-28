cf0302fc2367210031396018f0a97b311029f78d
Dan Bluhm Hansen <danx1314@edu.easj.dk>
2017-04-28 12:37:34 +0200

    Score fixed.

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -11,17 +11,16 @@ namespace Assets.Scripts
 		/// Start is called on the frame when a script is enabled just before
 		/// any of the Update methods is called the first time.
 		/// </summary>
-		void Start()
+		public void Start()
 		{
 			_scoreText = gameObject.GetComponent<Text>();
 		}
 
 		/// <summary>
-		/// This function is called when the object becomes enabled and active.
+		/// Update is called every frame, if the MonoBehaviour is enabled.
 		/// </summary>
-		public void OnEnable()
+		public void Update()
 		{
-			Debug.Log("adkjgnbakjdgbkadgf");
 			_scoreText.text = GameObject.FindWithTag("Player").GetComponent<PlayerController>().ScoreTime.ToString();
 		}
     }
